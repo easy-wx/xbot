a78c4f1565f692d20a9c73ce6896e9b97f581cdc
panzhongxian <panzhongxian0532@gmail.com>
2025-03-11 20:13:01 +0800

    Add long time process demo

--- spec_line_proc_funcs/hash_proc.py
+++ spec_line_proc_funcs/hash_proc.py
@@ -1,2 +1,15 @@
+import random
+import time
+
+
 def handle_command(user_id, msg, chat_id=None):
-    return "TODO: Implement line starting with '#' handling"
+    # TODO: Implement line starting with '#' handling"
+    random_number = random.choice([0, 1, 2])
+    if random_number == 0:
+        time.sleep(3)
+        return "Return after 3 seconds"
+    elif random_number == 1:
+        time.sleep(7)
+        return "Return after 7 seconds"
+    else:
+        return "Return immediately"
